f82885450b46571a3c983efdbea305b6ceca015b
nishida01092 <126932667+nishida01092@users.noreply.github.com>
2023-04-03 17:04:12 +0900

    初期設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react';
+import { cache } from '@emotion/css';
+import '../styles/globals.css';
+import '../styles/sp.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={cache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
